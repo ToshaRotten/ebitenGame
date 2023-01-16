b0fc2b0d00cf1c9ba6f26c260e3c957a090e3ee2
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-01-16 08:25:08 +0700

    refractor

--- camera/camera.go
+++ camera/camera.go
@@ -2,6 +2,7 @@ package camera
 
 import (
 	"ebitenGame/loger"
+	"ebitenGame/ui/menu"
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
@@ -21,6 +22,21 @@ func New() *Camera {
 
 func (c *Camera) UpdateCamera() *Camera {
 	loger.L.Trace("Update camera pos")
+	if menu.Mode == "EDITOR" {
+		if ebiten.IsKeyPressed(ebiten.KeyA) {
+			c.X -= 5
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyD) {
+			c.X += 5
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyS) {
+			c.Y += 5
+		}
+		if ebiten.IsKeyPressed(ebiten.KeyW) {
+			c.Y -= 5
+		}
+	}
+
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		c.X -= 5
 	}
