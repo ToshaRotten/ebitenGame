2e4399162c454a33cbe33cd55d125eb2977d7518
Tosha_Rotten <kozlovad@mer.ci.nsu.ru>
2023-01-17 02:45:04 +0700

    Интерфейс меню

--- ui/menu/menu.go
+++ ui/menu/menu.go
@@ -1,73 +1,34 @@
 package menu
 
 import (
-	c "ebitenGame/config"
-	"ebitenGame/ui"
-	u "ebitenGame/utils"
-	"github.com/hajimehoshi/ebiten/v2"
+	"ebitenGame/globals"
+	"ebitenGame/ui/elements"
 	"golang.org/x/image/colornames"
 )
 
-type Mouse struct {
-	X int
-	Y int
-}
+func GAME()        { globals.UI_MODE = "GAME" }
+func MULTIPLAYER() { globals.UI_MODE = "MULTIPLAYER" }
+func SETTINGS()    { globals.UI_MODE = "SETTINGS" }
+func EDITOR()      { globals.UI_MODE = "EDITOR" }
+func EXIT()        { globals.UI_MODE = "EXIT" }
 
-type Menu struct {
-	Buttons []*ui.Button
-}
-
-var (
-	mouse Mouse
-	Mode  = "MENU"
-)
-
-func START()       { Mode = "START" }
-func MULTIPLAYER() { Mode = "MULTIPLAYER" }
-func SETTINGS()    { Mode = "SETTINGS" }
-func EDITOR()      { Mode = "EDITOR" }
-func EXIT()        { Mode = "EXIT" }
-
-func MenuMouseCheck(m *Menu) {
-	mouse.X, mouse.Y = ebiten.CursorPosition()
-	//log.L.Debug(mouse.X, mouse.Y)
-	//log.L.Debug(u.InRange(int(m.Buttons[1].X), m.Buttons[1].Width, mouse.X) &&
-	//	u.InRange(int(m.Buttons[1].Y), m.Buttons[1].Height, mouse.Y))
-	for i := 0; i < len(m.Buttons); i++ {
-		if u.InRange(int(m.Buttons[i].X), int(m.Buttons[i].X)+m.Buttons[i].Width, mouse.X) &&
-			u.InRange(int(m.Buttons[i].Y), int(m.Buttons[i].Y)+m.Buttons[i].Height, mouse.Y) {
-			m.Buttons[i].BackgroundColor = colornames.Azure
-			if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
-				m.Buttons[i].OnClick()
-			}
-		} else {
-			m.Buttons[i].BackgroundColor = colornames.Palegreen
-		}
-	}
-
-}
-func New() *Menu {
-	var m Menu
-	PlayBtn := ui.NewButton(500, 100, float64(c.ScreenWidth/2-250), 0,
-		"Play", colornames.Palegreen, colornames.Black, START)
-	MultiplayerBtn := ui.NewButton(500, 100, float64(c.ScreenWidth/2-250), 150,
+func New() *elements.UI {
+	var m elements.UI
+	PlayBtn := elements.NewButton(500, 100, float64(globals.ScreenWidth/2-250), 0,
+		"Play", colornames.Palegreen, colornames.Black, GAME)
+	MultiplayerBtn := elements.NewButton(500, 100, float64(globals.ScreenWidth/2-250), 150,
 		"Multiplayer", colornames.Palegreen, colornames.Black, MULTIPLAYER)
-	SettingsBtn := ui.NewButton(500, 100, float64(c.ScreenWidth/2-250), 300,
+	SettingsBtn := elements.NewButton(500, 100, float64(globals.ScreenWidth/2-250), 300,
 		"Settings", colornames.Palegreen, colornames.Black, SETTINGS)
-	EditorBtn := ui.NewButton(500, 100, float64(c.ScreenWidth/2-250), 450,
+	EditorBtn := elements.NewButton(500, 100, float64(globals.ScreenWidth/2-250), 450,
 		"Editor", colornames.Palegreen, colornames.Black, EDITOR)
-	ExitBtn := ui.NewButton(500, 100, float64(c.ScreenWidth/2-250), 600,
+	ExitBtn := elements.NewButton(500, 100, float64(globals.ScreenWidth/2-250), 600,
 		"Exit", colornames.Palegreen, colornames.Black, EXIT)
+
 	m.Buttons = append(m.Buttons, PlayBtn)
 	m.Buttons = append(m.Buttons, MultiplayerBtn)
 	m.Buttons = append(m.Buttons, SettingsBtn)
 	m.Buttons = append(m.Buttons, EditorBtn)
 	m.Buttons = append(m.Buttons, ExitBtn)
 	return &m
-}
-
-func (m *Menu) Draw(screen *ebiten.Image) {
-	for i := 0; i < len(m.Buttons); i++ {
-		m.Buttons[i].Draw(screen)
-	}
 }
